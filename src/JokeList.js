669e39108f4bd7ae8a7ba97b4937f23258f3480c
charr1309 <ceh710@aol.com>
2021-05-23 22:23:10 -0400

    added static default props, create jokes array,use while loop to select 10 jokes

---
+++ src/JokeList.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class JokeList extends Component {
+    static defaultProps = {
+        numJokesToGet: 10
+    };
+    constructor(props) {
+        super(props);
+        this.state = {jokes: []};
+    }
+    async componentDidMount() {
+        let jokes = [];
+        while(jokes.length < this.props.numJokesToGet){
+            let res = await axios.get("https://icanhazdadjoke.com/", {
+                headers: {Accept: "application/json"}
+    });
+    jokes.push(res.data.joke);
+    
+    }
+    console.log(jokes);
+}
+    render () {
+        return (
+            <div>
+                <h1>Dad Jokes</h1>
+            </div>
+        );
+    }
+}
+ export default JokeList;
