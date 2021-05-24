46d7426d239903e70ad0eb10357d9a0107682d8e
charr1309 <ceh710@aol.com>
2021-05-23 23:51:46 -0400

    added emoji image and button to JokeList.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React, {Component} from 'react';
 import './App.css';
 import JokeList from './JokeList';
 
--- src/JokeList.js
+++ src/JokeList.js
@@ -1,36 +1,43 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class JokeList extends Component {
-    static defaultProps = {
-        numJokesToGet: 10
-    };
-    constructor(props) {
-        super(props);
-        this.state = {jokes: []};
+  static defaultProps = {
+    numJokesToGet: 10,
+  };
+  constructor(props) {
+    super(props);
+    this.state = { jokes: [] };
+  }
+  async componentDidMount() {
+    let jokes = [];
+    while (jokes.length < this.props.numJokesToGet) {
+      let res = await axios.get("https://icanhazdadjoke.com/", {
+        headers: { Accept: "application/json" },
+      });
+      jokes.push(res.data.joke); //res.data.joke is the path to a joke from the api
     }
-    async componentDidMount() {
-        let jokes = [];
-        while(jokes.length < this.props.numJokesToGet){
-            let res = await axios.get("https://icanhazdadjoke.com/", {
-                headers: {Accept: "application/json"}
-    });
-    jokes.push(res.data.joke);//res.data.joke is the path to a joke from the api
-    
-    }
-    this.setState({jokes: jokes});//will overwrite whatever is in the jokes array
+    this.setState({ jokes: jokes }); //will overwrite whatever is in the jokes array
+  }
+  render() {
+    return (
+      <div className="JokeList">
+          <div className='JokeList-sidebar'>
+              <h1 className='JokeList-title'>
+                  <span>Dad</span> Jokes
+              </h1>
+              <img
+                src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+                <button className='JokeList-getmore'>New Jokes</button>
+          </div>
+      
+        <div className="JokeList-jokes">
+          {this.state.jokes.map((j) => (
+            <div>{j}</div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
-    render () {
-        return (
-            <div className="JokeList">
-                <h1>Dad Jokes</h1>
-                <div className="JokeList-jokes">
-                    {this.state.jokes.map(j => (
-                        <div>{j}</div>
-                    ))}
-                </div>
-            </div>
-        );
-    }
-}
- export default JokeList;
+export default JokeList;
