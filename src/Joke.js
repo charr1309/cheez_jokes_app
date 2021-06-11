ecc9df25081acbfd60221810ef177acdda72dd44
charr1309 <ceh710@aol.com>
2021-06-11 03:46:24 -0400

    added vote buttons, added an emoji

--- src/Joke.js
+++ src/Joke.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
+import './joke.css';
 //Joke component is stateless, just calls on these icons an event 
 //handler that is passed down from the parent
 class Joke extends Component {
     render() {
-        return (
+        return (//used font awesome below to create icons for up and down arrows
             <div className='Joke'>
-                <div className='Joke-buttons'>
+                <div className='Joke-buttons'> 
                     <i className='fas fa-arrow-up' onClick={this.props.upvote} />
-                    <span>{this.props.votes}</span>
+                    <span className='Joke-votes'>{this.props.votes}</span>
                     <i className='fas fa-arrow-down' onClick={this.props.downvote} />
                 </div>
-                <div className='Joke-text'>
-                   {this.props.text} 
+                <div className='Joke-text'>{this.props.text}</div>
+                <div className = 'Joke-smiley'>
+                <i class="em em-rolling_on_the_floor_laughing" aria-role="presentation" aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
                 </div>
 
 
--- src/JokeList.js
+++ src/JokeList.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import './JokeList.css';
-import Joke from './Joke';
+import Joke from './Joke';//allows access to upvote and downvote passed to Joke.js from the render below
 import {v4 as uuidv4} from 'uuid';
 
 class JokeList extends Component {
@@ -13,20 +13,21 @@ class JokeList extends Component {
     this.state = { jokes: [] };
   }
   async componentDidMount() {
-    let jokes = [];
-    while (jokes.length < this.props.numJokesToGet) {
+    let jokes = [];//temporary array
+    while (jokes.length < this.props.numJokesToGet) {//set state once after getting 10 jokes--use while to handle duplicate jokes when we request 10 new jokes
       let res = await axios.get("https://icanhazdadjoke.com/", {
         headers: { Accept: "application/json" },
       });
-      jokes.push({id: uuidv4(), text: res.data.joke, votes: 0}); //res.data.joke is the path to a joke from the api
+      //call uuid as a function so with open and closing parenthesis
+      jokes.push({id: uuidv4(), text: res.data.joke, votes: 0}); //res.data.joke is the path to a joke from the api--making it an object allows other properties like votes to be added to each joke--using uuid provides an id for each joke so we can access them individualy in the hadleVote function
     }
     this.setState({ jokes: jokes }); //will overwrite whatever is in the jokes array
   }
-  handleVote(id, delta) {
+  handleVote(id, delta) {//delta could be a negative number or positive number--id tells which part of the state to update
       this.setState(
-          st =>({//implicit return so parenthesis wraps arrow function
+          st =>({//implicit return so parenthesis wraps arrow function--st is the variable holding the old state
               jokes: st.jokes.map(j =>
-                j.id === id ? {...j, votes: j.votes + delta} : j)
+                j.id === id ? {...j, votes: j.votes + delta} : j)//if the joke id is equal to the id that we pass in, take that joke and set its votes property to whatever it is plus delta
           
         }))
 
@@ -47,9 +48,10 @@ class JokeList extends Component {
           {this.state.jokes.map((j) => (
             // <div>{j.text} - {j.votes}</div> was mapping to a div
             <Joke 
-                key={j.id} 
+                key={j.id}//taken from the uuid given to each joke--needed to address key rule in React 
                 votes={j.votes} 
                 text={j.text} //changed to map from an instance of Joke
+                //below we have two methods that we have access too inside the props in the joke
                 upvote={()=> this.handleVote(j.id, 1)}
                 downvote={()=> this.handleVote(j.id, -1)}
             />
